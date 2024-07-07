2637c5dd17fcf23914fc9e642589804e9422d9a9
Bryan Gomes <bryanpgomes@gmail.com>
2024-07-07 19:26:40 -0400

    Add public static void main method

--- src/mortgagecalculator/Main.java
+++ src/mortgagecalculator/Main.java
@@ -1,13 +1,20 @@
 package mortgagecalculator;
+import java.util.Scanner;
 
-//Principal amount of loan 
-//Annual interest rate
-//Period in years calculates monthly payments for that loan
-//r = interest rate /12
-//n= number of payments in a year → years * 12
-//Monthly payment = Principal * ((1+r)^n/(1+r)^n-1)
 public class Main {
-//	Have user input principal amount of loan
-	//convert it to int and store it
-	
+
+	public static void main(String[] args) {
+		//Principal amount of loan 
+		//Annual interest rate
+		//Period in years calculates monthly payments for that loan
+		//r = interest rate /12
+		//n= number of payments in a year → years * 12
+		//Monthly payment = Principal * ((1+r)^n/(1+r)^n-1)
+//		Have user input principal amount of loan
+		//convert it to int and store it
+		Scanner scan = new Scanner(System.in);
+		int principal = scan.nextInt();
+
+	}
+
 }
