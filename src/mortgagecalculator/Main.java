82307ff3386ddc772baa3836df0bfbcc19b7fb5e
Bryan Gomes <bryanpgomes@gmail.com>
2024-07-07 19:12:27 -0400

    Added plan for app

--- src/mortgagecalculator/Main.java
+++ src/mortgagecalculator/Main.java
@@ -1,5 +1,13 @@
 package mortgagecalculator;
 
+//Principal amount of loan 
+//Annual interest rate
+//Period in years calculates monthly payments for that loan
+//r = interest rate /12
+//n= number of payments in a year → years * 12
+//Monthly payment = Principal * ((1+r)^n/(1+r)^n-1)
 public class Main {
+//	Have user input principal amount of loan
+	//convert it to int and store it
 	
 }
